ec059ceb6f67ebe29b7ba7b9ad8e75e3b5c10940
Ardouz11 <rachidardouz11@gmail.com>
2021-12-16 22:23:48 +0100

    Add Training ID To Form Values

--- src/app/training-details/training-details.component.ts
+++ src/app/training-details/training-details.component.ts
@@ -11,6 +11,7 @@ export class TrainingDetailsComponent implements OnInit {
   training_id;
   status;
   training_details;
+
   constructor(private restService:RestService,private activatedRoute:ActivatedRoute) { 
     this.training_id=this.activatedRoute.snapshot.params['id']
     console.log("the id passed is : ",this.activatedRoute.snapshot.params['id'])
@@ -27,6 +28,27 @@ export class TrainingDetailsComponent implements OnInit {
       console.log("status is after error ",this.status)
   })
   }
+  subscribeClient(subscribeForm){
+    //POST To add customer   
+    subscribeForm["id_training"]=this.training_id
+    console.log(subscribeForm);
+    this.restService.createCustomer(subscribeForm).subscribe((data:any)=>{
+       this.status=data.status
+       console.log("the resp is ",data)
+       console.log("Response code status is "+data.status+" and response is "+data.data);
+      // this.navigateToStatus(this.status)
+       //this.router.navigate(['home'])     
+    },error=>{
+        this.status=0
+        console.log("status is after error ",this.status)  
+       // this.navigateToStatus(this.status)
+      }
+       );
+        
+   
+   
+  }
+  
   ngOnInit(): void {
     this.getTrainingDetails()
   

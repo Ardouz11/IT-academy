895a59f57e1c09e8d16dd568e008fd5d5d17182b
Ardouz11 <rachidardouz11@gmail.com>
2022-02-12 21:32:58 +0100

    authorization

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 const baseUrlTrainings = 'https://itsren-academy.com/api/trainings';
 const baseUrlCustomer='https://itsren-academy.com/api/customer';
